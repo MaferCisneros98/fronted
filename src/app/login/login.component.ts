a4151eadc7843bc24b5cf97eb882eddfb2fdad1a
MaferCisneros98 <maria.cisneros.est@tecazuay.edu.ec>
2021-12-01 18:53:45 -0500

    se arregla token.service.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SocialAuthService, GoogleLoginProvider, FacebookLoginProvider, SocialUser } from 'angularx-social-login';
 import { Router } from '@angular/router';
-import {GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
-import { TokenDto } from '../models/token-dto';
 import { OauthService } from '../services/oauth.service';
 import { TokenService } from '../services/token.service';
+import { TokenDto } from '../models/token-dto';
 
 @Component({
   selector: 'app-login',
@@ -18,31 +18,32 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: SocialAuthService,
-    private router: Router, 
-    private oauthService:OauthService,
+    private router: Router,
+    private oauthService: OauthService,
     private tokenService: TokenService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.authService.authState.subscribe(
-      data=> {
+      data => {
         this.userLogged = data;
         this.isLogged = (this.userLogged != null && this.tokenService.getToken() != null);
       }
     );
   }
+
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
       data => {
         this.socialUser = data;
         const tokenGoogle = new TokenDto(this.socialUser.idToken);
         this.oauthService.google(tokenGoogle).subscribe(
-          res =>{
+          res => {
             this.tokenService.setToken(res.value);
             this.isLogged = true;
             this.router.navigate(['/']);
           },
-          err =>{
+          err => {
             console.log(err);
             this.logOut();
           }
@@ -56,12 +57,13 @@ export class LoginComponent implements OnInit {
   }
 
 
-logOut(): void{
-  this.authService.signOut().then(
-    data =>{
-      this.tokenService.logOut();
-      this.isLogged = false;
-    }
-  );
-}
+  logOut(): void {
+    this.authService.signOut().then(
+      data => {
+        this.tokenService.logOut();
+        this.isLogged = false;
+      }
+    );
+  }
+
 }
--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
 
-const TOKEN_KEY ='AuthToken';
+const TOKEN_KEY = 'AuthToken';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class TokenService {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
-  public setToken(token: string): void{
+  public getToken() {
+     return sessionStorage.getItem(TOKEN_KEY);
+
+  }
+
+  public setToken(token: string): void {
     sessionStorage.removeItem(TOKEN_KEY);
     sessionStorage.setItem(TOKEN_KEY, token);
   }
   
-  public getToken(): string {
-    return sessionStorage.getItem(TOKEN_KEY);
-  }
 
-  logOut(): void{
+  logOut(): void {
     sessionStorage.clear();
   }
 }
